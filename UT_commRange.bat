2af71a826ad7841e350d4cada0a2195fe0f0a628
lukas <lukas.mcclelland@gmail.com>
2020-04-15 07:31:31 -0400

    Logging gone. Starting unit tests

---
+++ UT_commRange.bat
@@ -0,0 +1,23 @@
+::             gui   #Bots #numEquip visionRadius commDelay timeout negPriority
+python main.py noGUI 2     50        50           100       10      -100
+python main.py noGUI 2     50        50           100       10      -100
+python main.py noGUI 2     50        50           100       10      -100
+python main.py noGUI 3     50        50           100       10      -100
+python main.py noGUI 3     50        50           100       10      -100
+python main.py noGUI 3     50        50           100       10      -100        True
+python main.py noGUI 5     50        50           100       10      -100        True
+python main.py noGUI 5     50        50           100       10      -100        True
+python main.py noGUI 5     50        50           100       10      -100        True
+::python main.py noGUI 10    50        50           100       10      -100        True
+::python main.py noGUI 10    50        50           100       10      -100        True
+::python main.py noGUI 10    50        50           100       10      -100        True
+::python main.py noGUI 20    50        50           100       10      -100        True
+::python main.py noGUI 20    50        50           100       10      -100        True
+::python main.py noGUI 20    50        50           100       10      -100        True
+::python main.py noGUI 50    50        50           100       10      -100        True
+::python main.py noGUI 50    50        50           100       10      -100        True
+::python main.py noGUI 50    50        50           100       10      -100        True
+::python main.py noGUI 100   50        50           100       10      -100        True
+::python main.py noGUI 100   50        50           100       10      -100        True
+::python main.py noGUI 100   50        50           100       10      -100        True
+pause
