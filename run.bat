972e3a6e3653008077477d395d490c6047b1c14e
Lukas McClelland <lukasmcclelland@cmail.carleton.ca>
2020-09-20 14:17:43 -0400

    Adjusted file/folder structure

--- run.bat
+++
@@ -1,2 +0,0 @@
-::             gui   #Bots #numEquip visionRadius commRange commDelay timeout negPriority enviroName outputFile
-python main.py noGUI 5     10        100          50        150       10      -100        black      results.csv
